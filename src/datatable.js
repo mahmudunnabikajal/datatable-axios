7d002fb2f471a02a1814a4853fffe5a1cb322ee7
Mahmudun Nabi Kajal <kajal@wpxpo.com>
2026-09-06 15:41:18 +0600

    chore(lint): add eslint and prettier configuration

--- src/datatable.js
+++ src/datatable.js
@@ -1,109 +1,83 @@
-// Import the axios instance from the local file "./axios"
-import axios from "./axios"
-
 /**
  * A datatable-axios class
  * @class
  */
 class datatable {
-    constructor() {
+  constructor() {}
 
+  /**
+   * Method to perform a GET request
+   * @param {string} url - The url of API endpoint
+   * @returns {Promise<object>} Server response from API endpoint
+   */
+  async get(url) {
+    // Parse the search parameters from the URL
+    let searchParams = new URLSearchParams(window.location.search);
+
+    // Retrieve values from search parameters
+    let page = searchParams.get("page");
+    let paginate = searchParams.get("paginate");
+    let search = searchParams.get("search");
+
+    // If no search parameters, make a regular GET request
+    if (!page && !paginate && !search) {
+      return window.axios.get(url);
     }
 
-    /**
-     * Method to perform a GET request
-     * @param {string} url - The url of API endpoint
-     * @returns {Promise<object>} Server response from API endpoint
-     */
-    async get(url) {
-        // Parse the search parameters from the URL
-        let searchParams = new URLSearchParams(window.location.search)
+    // If search parameters exist, append them to the URL
+    return window.axios.get(`${url}?page=${page}&paginate=${paginate}&search=${search}`);
+  }
 
-        // Retrieve values from search parameters
-        let page = searchParams.get('page')
-        let paginate = searchParams.get('paginate')
-        let search = searchParams.get('search')
+  /**
+   * Method to perform a POST request
+   *
+   * @async
+   * @param {string} url - The url of API endpoint
+   * @returns {Promise<object>} Server response from API endpoint
+   */
+  async post(url) {
+    // Parse the search parameters from the URL
+    let searchParams = new URLSearchParams(window.location.search);
 
-        // Return a Promise that resolves when the request completes
-        return new Promise(async (resolve) => {
-            if (!page && !paginate && !search) {
-                // If no search parameters, make a regular GET request
-                await window.axios.get(url).then(response => {
-                    resolve(response) // Resolve the Promise with the response
-                })
-            } else {
-                // If search parameters exist, append them to the URL
-                await window.axios.get(`${url}?page=${page}&paginate=${paginate}&search=${search}`).then(response => {
-                    resolve(response) // Resolve the Promise with the response
-                })
-            }
-        })
+    // Retrieve values from search parameters
+    let page = searchParams.get("page");
+    let paginate = searchParams.get("paginate");
+    let search = searchParams.get("search");
+
+    // If no search parameters, make a regular POST request
+    if (!page && !paginate && !search) {
+      return window.axios.post(url);
     }
 
-    /**
-     * Method to perform a POST request
-     * 
-     * @async
-     * @param {string} url - The url of API endpoint
-     * @returns {Promise<object>} Server response from API endpoint
-     */
-    async post(url) {
-        // Parse the search parameters from the URL
-        let searchParams = new URLSearchParams(window.location.search)
+    // If search parameters exist, append them to the URL
+    return window.axios.post(`${url}?page=${page}&paginate=${paginate}&search=${search}`);
+  }
 
-        // Retrieve values from search parameters
-        let page = searchParams.get('page')
-        let paginate = searchParams.get('paginate')
-        let search = searchParams.get('search')
+  /**
+   * Method to perform a PUT request
+   *
+   * @async
+   * @param {string} url - The url of API endpoint
+   * @returns {Promise<object>} Server response from API endpoint
+   */
+  async put(url) {
+    // Parse the search parameters from the URL
+    let searchParams = new URLSearchParams(window.location.search);
 
-        // Return a Promise that resolves when the request completes
-        return new Promise(async (resolve) => {
-            if (!page && !paginate && !search) {
-                // If no search parameters, make a regular POST request
-                await window.axios.post(url).then(response => {
-                    resolve(response) // Resolve the Promise with the response
-                })
-            } else {
-                // If search parameters exist, append them to the URL
-                await window.axios.post(`${url}?page=${page}&paginate=${paginate}&search=${search}`).then(response => {
-                    resolve(response) // Resolve the Promise with the response
-                })
-            }
-        })
+    // Retrieve values from search parameters
+    let page = searchParams.get("page");
+    let paginate = searchParams.get("paginate");
+    let search = searchParams.get("search");
+
+    // If no search parameters, make a regular PUT request
+    if (!page && !paginate && !search) {
+      return window.axios.put(url);
     }
 
-    /**
-     * Method to perform a PUT request
-     * 
-     * @async
-     * @param {string} url - The url of API endpoint
-     * @returns {Promise<object>} Server response from API endpoint
-     */
-    async put(url) {
-        // Parse the search parameters from the URL
-        let searchParams = new URLSearchParams(window.location.search)
-
-        // Retrieve values from search parameters
-        let page = searchParams.get('page')
-        let paginate = searchParams.get('paginate')
-        let search = searchParams.get('search')
-
-        // Return a Promise that resolves when the request completes
-        return new Promise(async (resolve) => {
-            if (!page && !paginate && !search) {
-                // If no search parameters, make a regular PUT request
-                await window.axios.put(url).then(response => {
-                    resolve(response) // Resolve the Promise with the response
-                })
-            } else {
-                // If search parameters exist, append them to the URL
-                await window.axios.put(`${url}?page=${page}&paginate=${paginate}&search=${search}`).then(response => {
-                    resolve(response) // Resolve the Promise with the response
-                })
-            }
-        })
-    }
+    // If search parameters exist, append them to the URL
+    return window.axios.put(`${url}?page=${page}&paginate=${paginate}&search=${search}`);
+  }
 }
 
 // Export the "datatable" class as the default export
-export default datatable
+export default datatable;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Import the "datatable" class from the local file "./datatable"
-import datatable from './datatable'
+import datatable from "./datatable";
 
 // Export the "datatable" class as the default export
-export default datatable
+export default datatable;
